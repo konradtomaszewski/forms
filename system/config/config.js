a7ddda45a83c97383ba3c78bbeb67079568c4792
konradtomaszewski <konrad.tomaszewski85@gmail.com>
2017-06-25 13:36:37 +0200

    przebudowa struktury plików i routing

---
+++ system/config/config.js
@@ -0,0 +1,36 @@
+
+	var app = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages']);
+		app.config(['$routeProvider',
+				function($routeProvider) {
+					$routeProvider
+						.when('/forms', {
+							title: 'Forms',
+							templateUrl: 'application/views/forms/forms.html',
+							controller: 'DemoCtrl'
+						})
+						.when('/showCharInfo/:personName', {
+							title: 'Szczegóły rekordu',
+							templateUrl: 'charInfo.html',
+							controller: 'people'
+						})
+						.when('/', {
+							title: 'index',
+							templateUrl: 'application/views/start.html',
+							controller: ''
+						})
+						.otherwise({
+							title: '404',
+							templateUrl: 'application/views/404.html',
+							controller: ''
+						})
+		}]);
+		app.run(['$location', '$rootScope', function($location, $rootScope) {
+				$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+
+					if (current.hasOwnProperty('$$route')) {
+
+						$rootScope.title = current.$$route.title;
+					}
+				})
+		}]);
+		
