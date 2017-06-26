0b536d151175b891c35a83577002ab00bcf93836
konradtomaszewski <konrad.tomaszewski85@gmail.com>
2017-06-26 21:24:15 +0200

    inny skrypt topMenu

--- application/controllers/topMenu.js
+++ application/controllers/topMenu.js
@@ -3,62 +3,13 @@
 
  function PanelGroupsCtrl($mdPanel,$scope,$location) {
     this.settings = {
-      name: 'settings',
-      items: ['Home','Forms']
-    };
+		name: ['Menu'],
+		items: ['Home','Forms']
+	};
 	this.more = {
-      name: 'settings',
-      items: ['Wyloguj']
-    };
-	
-   this.menuTemplate = '' +
-        '<div class="menu-panel" md-whiteframe="4">' +
-        '  <div class="menu-content">' +
-        '    <div class="menu-item" ng-repeat="item in ctrl.items">' +
-        '        <a class="md-button" ng-click="ctrl.closeMenu()" href="#/{{item}}">{{item}}</a>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>';
-
-    $mdPanel.newPanelGroup('toolbar', {
-      maxOpen: 2
-    });
-
-    $mdPanel.newPanelGroup('menus', {
-      maxOpen: 3
-    });
-
-    this.showToolbarMenu = function($event, menu) {
-      var template = this.menuTemplate;
-
-      var position = $mdPanel.newPanelPosition()
-          .relativeTo($event.srcElement)
-          .addPanelPosition(
-            $mdPanel.xPosition.ALIGN_START,
-            $mdPanel.yPosition.BELOW
-          );
-
-      var config = {
-        id: 'toolbar_' + menu.name,
-        attachTo: angular.element(document.body),
-        controller: PanelMenuCtrl,
-        controllerAs: 'ctrl',
-        template: template,
-        position: position,
-        panelClass: 'menu-panel-container',
-        locals: {
-          items: menu.items
-        },
-        openFrom: $event,
-        focusOnOpen: true,
-        zIndex: 100,
-        propagateContainerEvents: true,
-        groupName: ['toolbar', 'menus']
-      };
-
-      $mdPanel.open(config);
-    };
-
+		name: ['User'],
+		items: ['Wyloguj']
+	};
   }
 
   function PanelMenuCtrl(mdPanelRef, $location,) {
