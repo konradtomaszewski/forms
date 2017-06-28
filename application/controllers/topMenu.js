8bca3a86d091f9b832547d18ca078bca602ec336
konradtomaszewski <konrad.tomaszewski85@gmail.com>
2017-06-28 15:53:44 +0200

    sidebar

--- application/controllers/topMenu.js
+++ application/controllers/topMenu.js
@@ -1,15 +1,27 @@
   app.controller('PanelGroupsCtrl', PanelGroupsCtrl);
   app.controller('PanelMenuCtrl', PanelMenuCtrl);
 
- function PanelGroupsCtrl($mdPanel,$scope,$location) {
+ function PanelGroupsCtrl($mdPanel,$scope,$location, $timeout, $mdSidenav) {
     this.settings = {
 		name: ['Menu'],
 		items: ['Home','Forms']
 	};
-	this.more = {
-		name: ['User'],
-		items: ['Wyloguj']
-	};
+	this.user = [
+		{
+			icon: 'settings_b.svg',
+			menu: 'Ustawienia'
+		},
+		{
+			icon: 'logout.svg',
+			menu: 'Wyloguj'
+		}
+		
+	];
+	//$scope.toggleLeft = toggleLeft('left');
+	 
+	this.toggleRight = function() {
+        $mdSidenav('right').toggle();
+    };
   }
 
   function PanelMenuCtrl(mdPanelRef, $location,) {
